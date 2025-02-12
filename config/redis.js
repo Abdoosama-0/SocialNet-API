685f615ec5a7238c93545201dfe4c55f5a7d48c8
abdoosama-0 <abdalrhman.osama.mostafa@gmail.com>
2025-02-12 13:46:15 +0200

    4

--- config/redis.js
+++ config/redis.js
@@ -2,9 +2,8 @@ const Redis = require("ioredis");
  
 
 const redis = new Redis(process.env.REDIS_URL);
-/**    
- * host: process.env.REDIS_HOST || "127.0.0.1",//"localhost"
-    port: process.env.REDIS_PORT || 6379,
+/**    { host:  "127.0.0.1",
+    port:  6379,}
      */
 redis.on("connect", () => {
     console.log("✅ Connected to Redis ");
